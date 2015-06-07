90cbfbf5fd11b856b8577f03f24a3fe7c94cf407
Jess Mann <jmann@smith.edu>
2015-06-06 20:04:42 -0400

    odd sum of two numbers; sum is tentatively interpreted as exclusive of end integer [start, end)

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,20 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+
+        int sum = 0;
+
+        // if the starting number is not odd, increment by 1 to make it odd
+        if(start%2==0){
+            start++;
+        }
+
+        // assuming that the sum is [start, end) -- not inclusive the end integer
+        while(start < end){
+            sum+=start;
+            start += 2;
+        }
+
+        return sum;
     }
 }
